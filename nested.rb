b57297f78f574b76f031665d370043f6f91c8c25
Austin Evans <unity7591@gmail.com>
2020-05-03 03:43:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -18,10 +18,7 @@ def hopper
 programmer_hash[:grace_hopper]
 end
 
-
-
 def alan_kay_is_known_for
-	
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -83,9 +80,6 @@ def adding_matz
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -106,7 +100,6 @@ def changing_alan
 end
 
 def adding_to_dennis
-
 	programmer_hash = 
  		{
       :grace_hopper => {
